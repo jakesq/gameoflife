67a8126fa568b159db1985e69004e37a817821bb
Jake Bailey <31568686+jakesq@users.noreply.github.com>
2022-07-02 20:27:57 +0100

    Add files via upload

--- GameOfLife.java
+++ GameOfLife.java
@@ -1,45 +1,98 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.util.ArrayList;
 import javax.swing.*;
 
-public class GameOfLife extends JFrame { 
-    JPanel panel = new JPanel();
-    JButton refresh = new JButton("Next Generation");
-    public int gameHeight = 900;
-    public int gameWidth = 900;
+public class GameOfLife extends JFrame {
+    int gens = 0;
+    long speed = 5;
 
-    int[][] matrix = new int[gameHeight/10][gameWidth/10];
+    static JPanel panel = new JPanel();
+    JButton nextButton = new JButton("Next Generation");
+    JButton toggleGridButton = new JButton("Toggle Grid");
+//    JSlider speedSlider = new JSlider();
+    public int gameHeight = 400;
+    public int gameWidth = 400;
+    boolean showGrid = true;
+    int[][] matrix;
+    int[][] tempMatrix = new int[gameWidth / 10][gameHeight / 10];
+
+    int widthOffset = 1;
+    int heightOffset = 31;
+
+    private Graphics2D g;
+
+    //sets pointer position
+    int[][] intMatrix = {{-1,-1}, {0,-1}, {1,-1},
+                         {-1,0},          {1,0}, //(1,1) isn't present as the current position shouldn't be evaluated
+                         {-1,1},  {0,1},  {1,1}};
 
     public GameOfLife() {
         super("Game of Life");
-        add(panel);
+        matrix = new int[gameHeight / 10][gameWidth / 10];
         setupWindow();
+        add(panel);
+        setResizable(false); //resizing causes the app to freeze
     }
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         GameOfLife gameOfLife = new GameOfLife();
         gameOfLife.setVisible(true);
     }
 
     private void setupWindow() {
-        setSize(gameWidth+1, gameHeight+31);
-        panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
-        panel.add(refresh);
-        refresh.setAlignmentY(Component.BOTTOM_ALIGNMENT);
+        setSize(gameWidth + widthOffset, gameHeight + heightOffset);
+        panel.setLayout(new BoxLayout(panel, 0));
+        panel.add(nextButton);
+        panel.add(toggleGridButton);
+//        panel.add(speedSlider);
+        nextButton.setAlignmentY(1.0F);
+        toggleGridButton.setAlignmentY(1.0F);
+//        speedSlider.setAlignmentY(1.0F);
+//        speedSlider.setValue(Integer.valueOf((int) speed*5));
 
-        addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent windowEvent) {
-                System.exit(0);
+        panel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                System.out.println(e.getPoint());
+                Point point = e.getPoint();
+
+                if (matrix[(point.y+heightOffset)/10][(point.x+widthOffset)/10] != 1)
+                    matrix[(point.y+heightOffset)/10][(point.x+widthOffset)/10] = 1;
+                else
+                    matrix[(point.y+heightOffset)/10][(point.x+widthOffset)/10] = 0;
+
+                repaint();
             }
         });
+
+        nextButton.addActionListener(e -> {
+            generation(g, 1, speed, showGrid);
+            repaint();
+        });
+
+        toggleGridButton.addActionListener(e -> {
+            if (showGrid)
+                showGrid = false;
+            else
+                showGrid = true;
+
+            clearGrid(g);
+            repaint();
+        });
+
+//        speedSlider.addChangeListener(e -> {
+//            speed = speedSlider.getValue();
+//        });
     }
 
     public void setGlider(int offset) {
-        matrix[22+offset][20+offset] = 1;
-        matrix[23+offset][21+offset] = 1;
-        matrix[21+offset][22+offset] = 1;
-        matrix[22+offset][22+offset] = 1;
-        matrix[23+offset][22+offset] = 1;
+        matrix[22 + offset][20 + offset] = 1;
+        matrix[23 + offset][21 + offset] = 1;
+        matrix[21 + offset][22 + offset] = 1;
+        matrix[22 + offset][22 + offset] = 1;
+        matrix[23 + offset][22 + offset] = 1;
     }
 
     public void setBeacon() {
@@ -47,165 +100,144 @@ public class GameOfLife extends JFrame {
         matrix[11][10] = 1;
         matrix[10][11] = 1;
         matrix[11][11] = 1;
-
         matrix[12][12] = 1;
         matrix[13][12] = 1;
         matrix[12][13] = 1;
         matrix[13][13] = 1;
     }
 
-    public void alive(int[][] tempMatrix, int x, int y) {
-        if ((x > 3 && y > 3) && (x < gameWidth/10-3 && y < gameHeight/10-3)) {
-            if (tempMatrix[x][y] != 1) {
-                int neighbors = 0;
+    public int birth(int x, int y) {
+        int neighbors = 0;
 
-                if (tempMatrix[x-1][y-1] == 1) { //row above cell
-                    neighbors ++;
-                } if (tempMatrix[x][y-1] == 1) {
-                    neighbors ++;
-                } if (tempMatrix[x+1][y-1] == 1) {
-                    neighbors ++;
+        for (int i = 0; i < intMatrix.length; i++) {
+            try {
+                if (tempMatrix[x+intMatrix[i][0]][y+intMatrix[i][1]] == 1) {
+                    neighbors++;
                 }
-    
-                if (tempMatrix[x-1][y] == 1) { //cell's current row
-                    neighbors ++;
-                }  if (tempMatrix[x+1][y] == 1) {
-                    neighbors ++; 
-                }
-    
-                if (tempMatrix[x-1][y+1] == 1) { //row below cell
-                    neighbors ++;
-                } if (tempMatrix[x][y+1] == 1) {
-                    neighbors ++;
-                } if (tempMatrix[x+1][y+1] == 1) {
-                    neighbors ++;
-                }
-    
-                if (neighbors == 3) {
-                    matrix[x][y] = 1;
-                }
+            } catch (Exception e) {
+//                e.printStackTrace();
             }
         }
+
+        if (neighbors == 3) {
+            matrix[x][y] = 1;
+        }
+
+        return neighbors;
     }
 
-    public void dead(int[][] tempMatrix, int x, int y) {
-        if ((x > 3 && y > 3) && (x < gameWidth/10-3 && y < gameHeight/10-3)) {
-            if (tempMatrix[x][y] == 1) {
-                int neighbors = 0;
+    public int kill(int x, int y) {
+        int neighbors = 0;
 
-                if (tempMatrix[x-1][y-1] == 1) { //row above cell
-                    neighbors ++;
-                } if (tempMatrix[x][y-1] == 1) {
-                    neighbors ++;
-                } if (tempMatrix[x+1][y-1] == 1) {
-                    neighbors ++;
+        for (int i = 0; i < intMatrix.length; i++) {
+            try {
+                if (tempMatrix[x+intMatrix[i][0]][y+intMatrix[i][1]] == 1) {
+                    neighbors++;
                 }
-    
-                if (tempMatrix[x-1][y] == 1) { //cell's current row
-                    neighbors ++;
-                }  if (tempMatrix[x+1][y] == 1) {
-                    neighbors ++; 
-                }
-    
-                if (tempMatrix[x-1][y+1] == 1) { //row below cell
-                    neighbors ++;
-                } if (tempMatrix[x][y+1] == 1) {
-                    neighbors ++;
-                } if (tempMatrix[x+1][y+1] == 1) {
-                    neighbors ++;
-                }
-    
-                if (neighbors > 3 || neighbors < 2) {
-                    matrix[x][y] = 0;
-                }
+            } catch (Exception e) {
+//                e.printStackTrace();
             }
         }
+
+        if (neighbors > 3 || neighbors < 2) {
+            matrix[x][y] = 0;
+        }
+
+        return neighbors;
     }
 
-    public void runState(Graphics g) {
-        int[][] tempMatrix = new int[gameWidth/10][gameHeight/10];
-        for(int i = 0; i < matrix.length; i++) {
+    public void runState(Graphics g, boolean showGrid) {
+        for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
                 tempMatrix[i][j] = matrix[i][j];
             }
         }
+
         for (int i = 0; i < tempMatrix.length; i++) {
             for (int j = 0; j < tempMatrix[i].length; j++) {
-                dead(tempMatrix, j, i);
-                alive(tempMatrix, j, i);
+                if (tempMatrix[j][i] == 1) {
+                    kill(j, i);
+                } else {
+                    birth(j, i);
+                }
             }
         }
-        smartCellRedraw(g, matrix, tempMatrix);
+
+        smartCellRedraw(g, matrix, tempMatrix, showGrid);
     }
 
-    public void generation(Graphics g, int generations) {
-        Thread thread = new Thread();
-        try {
-            for (int x = 0; x < generations; x++) {
-                thread.sleep(1000);
-                runState(g);
+    public void generation(Graphics g, int generations, long speed, boolean showGrid) {
+        gens = generations;
+        for (int x = 0; x < generations; x++) {// try/catch necessary for thread sleep
+            gens--;
+            System.out.println(gens);
+            try {
+                runState(g, showGrid);
+                Thread.sleep(speed * 10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
         }
     }
 
     public void clearGrid(Graphics g) {
         for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix.length; j++) {
-                g.clearRect(j, i, 10, 10);
+            for (int j = 0; j < matrix[i].length; j++) {
+                g.clearRect(j * 10, i * 10, 10, 10);
             }
         }
     }
 
-    public void drawGrid(Graphics g) {
+    public void drawGrid(Graphics g, boolean showGrid) {
         clearGrid(g);
+
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
-                g.drawRect(j*10, i*10, 10, 10);
                 if (matrix[i][j] == 1) {
-                    g.fillRect(j*10, i*10, 10, 10);
+                    g.fillRect(j * 10, i * 10, 10, 10);
+                }
+
+                if (showGrid) {
+                    g.drawRect(j * 10, i * 10, 10, 10);
                 }
             }
         }
     }
 
-    public void smartCellRedraw(Graphics g, int[][] oldMatrix, int[][] newMatrix) {
+
+    public void smartCellRedraw(Graphics g, int[][] oldMatrix, int[][] newMatrix, boolean showGrid) {
         for (int i = 0; i < oldMatrix.length; i++) {
             for (int j = 0; j < oldMatrix[i].length; j++) {
                 if (oldMatrix[i][j] != newMatrix[i][j]) {
                     if (newMatrix[i][j] == 1) {
-                        g.clearRect(j*10, i*10, 10, 10);
-                        g.drawRect(j*10, i*10, 10, 10);
+                        g.clearRect(j * 10, i * 10, 10, 10);
+                        if (showGrid) {
+                            g.drawRect(j * 10, i * 10, 10, 10);
+                        }
                     } else if (oldMatrix[i][j] == 1) {
-                        g.fillRect(j*10, i*10, 10, 10);
+                        g.fillRect(j * 10, i * 10, 10, 10);
                     }
                 }
             }
-        } 
+        }
     }
-    
+
+
     @Override
-    public void paint(Graphics g) {
-        Graphics2D g2 = (Graphics2D) g;
-        g2.setClip(0, 30, gameWidth+1, gameHeight+31);
-        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-            RenderingHints.VALUE_ANTIALIAS_ON);
-        
-        setGlider(0);
-        setGlider(4);
-        setGlider(8);
-        setGlider(12);
-        setGlider(12);
-        setBeacon();
-        drawGrid(g2); 
-        generation(g2, 500);
-        refresh.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("Refreshing");
-                generation(g, 10);
-            }
-        });
+    public void paint(Graphics graphics) {
+        if (g == null) {
+            g = (Graphics2D) graphics;
+            g.setClip(0, 30, gameWidth + 1, gameHeight + 31);
+            setGlider(0);
+            setGlider(4);
+            setGlider(8);
+            setGlider(12);
+            setBeacon();
+            drawGrid(g, showGrid);
+        } else {
+            g = (Graphics2D) graphics;
+            drawGrid(g, showGrid);
+//            g.dispose(); //cell artifacts remain if not disposed
+        }
     }
 }
--- README.md
+++ README.md
@@ -2,9 +2,15 @@
 (WIP) A Java implementation of Conway's Game of Life
 https://en.wikipedia.org/wiki/Conway's_Game_of_Life
 
-The code is a bit messy, and should use a better cell-proximity checker for each generation so that the outer 3x3 cell border is usable space.
+Issues:
+- Buttons won't appear unless you click outside the window, then back inside.
 
-I will be updating this in my spare time, adding new features and refactoring the code as I become more comfortable with Java.
+- You can click each cell to kill/birth, but the calibration isn't perfect so you'll need to click in the very centre for accuracy.
 
-#### Runtime on macOS 12.0
-![window](https://raw.githubusercontent.com/jakesq/gameoflife/main/window.png)
+- Resizing the app during a generation may cause it to crash (so it's been disabled)
+
+- The top of the matrix is cut off, with at least one row being obscured by the title bar
+
+- Gen progression causes the last line of the matrix to flicker, as all repaints currently require the app to redraw.
+This is likely tied to CPU speed, I need implement the smart redraw method from elsewhere in the app.  Also, if grid is toggled off each living
+cell will flicker due to the same repaint redraw issue (I presume).
